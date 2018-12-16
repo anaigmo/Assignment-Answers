8eb1b6275f88ee45ab99911d8328e0ab2ff4ac76
anaigmo <ana.iglesias.molina@gmail.com>
2018-12-16 19:54:14 +0100

    Comentado y terminado?

--- Assignment_4/BRH.rb
+++ Assignment_4/BRH.rb
@@ -1,18 +1,31 @@
+## ------------------------------------------- Assignment 4 -------------------------------------------  ##
+#
+# This program has been created by Ana Iglesias Molina for the subject Bioinformatics Programming Challenges.
+#
+# This program takes two multifasta files as databases, does blast against one another, saves the reciprocal
+# best hits and prints it in a file called 'Reciprocal_best_hits.tsv'.
+# The criteria to accept a best hit is an e-value of 1e-10 and an overlap length of at least half the length
+# of the query sequence. I have chosen those values according to what I have read in the literature. The
+# e-value is quite tough, but above 1e-05 it seems it is not robust enough, and under 1e-10 are considered
+# closed related sequences. Regarding the overlapping length, it's the most common value I've seen. 70-75%
+# of the length of the sequence could be also used, but since the e-value is strict, I decided no to be so in
+# ovelapping length. 
+
 require 'bio'
 require 'stringio'
 
 
-## makeblastdb -in short_db/rep_TAIR10_short_seq.fa -dbtype 'nucl' -out short_db/rep_TAIR10_short_seq
-
 ## ----- FUNCTIONS ----- ##
 
 def check_type_sequence(database)
+  # This function takes a multifasta file, extracts the first sequence and finds out the type of
+  # sequences it contains. It returns 'AA' for aminoacids and 'NA' for nucleotides.
   
   seq = []
   first = true
-  File.open(database, "r").each do |line|
+  File.open(database, 'r').each do |line|
     
-    if line[0] == '>' and not first
+    if line[0] == '>' and not first  # finish in second sequence
       break
     elsif first
       first = false
@@ -25,25 +38,30 @@ def check_type_sequence(database)
   
   seq = seq.join('')
   
-  seq = Bio::Sequence.auto(seq)
+  seq = Bio::Sequence.auto(seq)  # Transforms the sequence in a Bio::Sequence object
   
-  return(seq.guess.to_s.split('::')[-1])
+  return(seq.guess.to_s.split('::')[-1])  # Returns 'AA' or 'NA'
 
 end
 
 
 
 def create_factory(name_db, type)
+  # This functions creates a database for blast from a multifasta file 'name_db'. It needs
+  # to know if the database contains protein or nucleotide sequences. It creates de files in
+  # the directory the multifasta is with a shell command, and then creates the factory object
+  # and returns it. 
   
   raw_name = name_db.split('.')[0]  # Remove the extension
   
+  # Create the necessary files to create the factory object
   if type == 'blastp' or type == 'blastx'
     `makeblastdb -in #{name_db} -dbtype 'prot' -out #{raw_name}`
   elsif type == 'blastn' or type == 'tblastn'
     `makeblastdb -in #{name_db} -dbtype 'nucl' -out #{raw_name}`
   end
   
-  factory = Bio::Blast.local(type, raw_name)
+  factory = Bio::Blast.local(type, raw_name)  
   
   return factory
   
@@ -52,6 +70,8 @@ end
 
 
 def create_query_db(name_db)
+  # This function creates an object to iterate every sequence in a multifasta file 'name_db'
+  # and returns it
   
   query_db = Bio::FlatFile.auto(name_db)
   
@@ -61,35 +81,11 @@ end
 
 
 
-def find_best_hit(seq_query, factory)
-  
-  report = factory.query(seq_query)  # blast of query sequence in factory db
-    
-    first = true
-    best_hit = false
-    report.each_hit do |hit|
-      if first 
-        best_hit = hit
-        #first = false  ESTO HABRÍA QUE PENSARLO BIEN, SI NO CON BREAK VA UN PELIN MAS RAPIDO
-        break
-      elsif hit.evalue < best_hit.evalue  # if another hit is better than the first
-        best_hit = hit
-      end
-    end
-    
-    if best_hit and best_hit.evalue < 1e-05  # minimun e-value to be considered significant
-      id = seq_query.definition.split('|')[0]
-      bh_id = best_hit.definition.split('|')[0]
-      return [id, bh_id]
-    else
-      return false
-    end
-end
-
-
-
 def find_reciprocal_best_hits(db1, db2)
+  # This function does blast to both databases from the other to find the reciprocal best hits.
+  # It takes the names of the databases and returns a hash with the correspondance of genes.
   
+  # First, it creates the objects query and factory to do the blast. 
   type1 = check_type_sequence(db1)
   type2 = check_type_sequence(db2)
   
@@ -115,35 +111,52 @@ def find_reciprocal_best_hits(db1, db2)
   
   
   # First search
+  # The results of the best hits in this search are saved in two ways. The query gen as key and its best
+  # hit as value in the hash called 'best_hits1', and the best hit as key in the hash 'best_hits2'.
+  # Having all the possible keys saved in best_hits2 will save time in the next search. 
   
   best_hits1 = {}
   best_hits2 = {}
   query_db1.each do |seq|  # looks for best hit in factory db for each sequence in query_db
       
-    best_hit = find_best_hit(seq, factory1)
+    report = factory1.query(seq)  # blast of query sequence in factory db
     
-    if best_hit
-      best_hits1[best_hit[0]] = best_hit[1] if best_hit  # stores the gene id correspondance
-      best_hits2[best_hit[1]] = false  # To reduce the search on the next database
+    # Restrictions to alignments: evalue under 1e-10 and alignment length over half the length of query seq
+    next unless report.hits[0] and report.hits[0].evalue < 1e-10 and report.hits[0].overlap > seq.length/2
+    
+    # Get id of genes
+    id_query = seq.definition.split('|')[0]
+    id_bh = report.hits[0].definition.split('|')[0]
+    
+    best_hits1[id_query] = id_bh   # stores the gene id correspondance
+    best_hits2[id_bh] = false  # To reduce the search in the next database
       
-      puts("#{best_hit[0]} -> #{best_hit[1]}")  ## ESTO SE PUEDE QUITAR
-    end
-        
+    puts("Best hit found: #{id_query} -> #{id_bh}")
+      
   end
   
   
   # Second search
-  
+  # The best hits are saved ase value of its correspondent key in 'best_hits2'
+
   query_db2.each do |seq|
     
-    # Doesn't do blast if is not a best hit in the first list
-    next unless best_hits2.has_key?(seq.definition.split('|')[0])
-    best_hit = find_best_hit(seq, factory2)
-    best_hits2[best_hit[0]] = best_hit[1] if best_hit  # stores the gene id correspondance
+    # Doesn't do blast if the sequence is not a best hit in the first list
+    next unless best_hits2.has_key?(seq.definition.split('|')[0]) 
+    
+    report = factory2.query(seq)  # blast of query sequence in factory db
+    next unless report.hits[0] and report.hits[0].evalue < 1e-10 and report.hits[0].overlap > seq.length/2
       
-    puts("#{best_hit[0]} -> #{best_hit[1]}") if best_hit  ## ESTO SE PUEDE QUITAR
+    id_query = seq.definition.split('|')[0]
+    id_bh = report.hits[0].definition.split('|')[0]
+    
+    best_hits2[id_query] = id_bh   # stores the gene id correspondance
+      
+    puts("Best hit found: #{id_query} -> #{id_bh}")  ## ESTO SE PUEDE QUITAR
+   
   end
   
+  # Checks reciprocal best hits in both best hits hashes
   rec_best_hits = {}
   
   best_hits1.each_key do |key|
@@ -157,7 +170,11 @@ def find_reciprocal_best_hits(db1, db2)
 end
 
 
+
 def write_repot(rec_best_hits, db1_name, db2_name)
+  # This function prints in a tabular format file the reciprocal best hits found
+  
+  puts("The reciprocal best hits found will be saved in Reciprocal_best_hits.tsv")
   
   out_file = File.open("Reciprocal_best_hits.tsv", "w")
   out_file.puts("#{db1_name}\t#{db2_name}")
@@ -176,18 +193,9 @@ abort ("Missing proteome files.") unless ARGV.length == 2
 db1_name = ARGV[0]
 db2_name = ARGV[1]
 
+# Find reciprocal best hits
+puts("Finding best reciprocal hits, it will take a while. ")
 rec_best_hits = find_reciprocal_best_hits(db1_name, db2_name)
 
+# And print it in a file called 'Reciprocal_best_hits.tsv
 write_repot(rec_best_hits, db1_name, db2_name)
-
-
-
-
-
-
-
-
-
-
-
-
