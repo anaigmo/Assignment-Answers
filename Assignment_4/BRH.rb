bb3011fba6c0a6330bce8e838519dec5fffae7c6
anaigmo <ana.iglesias.molina@gmail.com>
2018-12-18 12:53:42 +0100

    Overlap and identity. Bonus?

--- Assignment_4/BRH.rb
+++ Assignment_4/BRH.rb
@@ -4,12 +4,12 @@
 #
 # This program takes two multifasta files as databases, does blast against one another, saves the reciprocal
 # best hits and prints it in a file called 'Reciprocal_best_hits.tsv'.
-# The criteria to accept a best hit is an e-value of 1e-10 and an overlap length of at least half the length
-# of the query sequence. I have chosen those values according to what I have read in the literature. The
-# e-value is quite tough, but above 1e-05 it seems it is not robust enough, and under 1e-10 are considered
-# closed related sequences. Regarding the overlapping length, it's the most common value I've seen. 70-75%
-# of the length of the sequence could be also used, but since the e-value is strict, I decided no to be so in
-# ovelapping length. 
+# The criteria to accept a best hit is an e-value of 1e-6, an overlap length of at least half the length
+# of the query sequence and a percent of identity of 30%. I have chosen those values according to what
+# I have read in the literature. The e-value is not tough, but above 1e-05 it seems it is not robust enough,
+# and under 1e-6 is considered necesary for nucleotide blast. Regarding the overlapping length, it's the most
+# common value I've seen. 70-75% of the length of the sequence could be also used, but for a first approximation
+# to look for orthologues I preferred not to be so tough, same happens with the value of identity.
 
 require 'bio'
 require 'stringio'
@@ -95,18 +95,22 @@ def find_reciprocal_best_hits(db1, db2)
   if type1 == type2 and type2 == 'AA'
     factory1 = create_factory(db2, 'blastp')
     factory2 = create_factory(db1, 'blastp')
+    blast_case = 1
     
   elsif type1 == type2 and type2 == 'NA'
     factory1 = create_factory(db2, 'blastn')
     factory2 = create_factory(db1, 'blastn')
+    blast_case = 1
     
   elsif type1 == 'NA' and type2 == 'AA'
     factory1 = create_factory(db2, 'blastx')
     factory2 = create_factory(db1, 'tblastn')
+    blast_case = 2
     
   elsif type1 == 'AA' and type2 == 'NA'
     factory1 = create_factory(db2, 'tblastn')
     factory2 = create_factory(db1, 'blastx')
+    blast_case = 3
   end
   
   
@@ -121,8 +125,19 @@ def find_reciprocal_best_hits(db1, db2)
       
     report = factory1.query(seq)  # blast of query sequence in factory db
     
-    # Restrictions to alignments: evalue under 1e-10 and alignment length over half the length of query seq
-    next unless report.hits[0] and report.hits[0].evalue < 1e-10 and report.hits[0].overlap > seq.length/2
+    # Restrictions to alignments: evalue under 1e-6
+    next unless report.hits[0] and report.hits[0].evalue < 1e-6
+    
+    if blast_case == 1 or blast_case == 3
+      percent_overlap = report.hits[0].overlap/seq.length.to_f
+      percent_identity = report.hits[0].identity/seq.length.to_f
+    elsif blast_case == 2
+      percent_overlap = report.hits[0].overlap*3/seq.length.to_f
+      percent_identity = report.hits[0].identity*3/seq.length.to_f
+    end
+    
+    # More restrictions to alignments: overlap over 50% and identity over 30%
+    next unless percent_overlap >= 0.5 and percent_identity >= 0.3
     
     # Get id of genes
     id_query = seq.definition.split('|')[0]
@@ -132,6 +147,8 @@ def find_reciprocal_best_hits(db1, db2)
     best_hits2[id_bh] = false  # To reduce the search in the next database
       
     puts("Best hit found: #{id_query} -> #{id_bh}")
+    puts("%overlap: #{percent_overlap}\t%identity: #{percent_identity}\tevalue: #{report.hits[0].evalue}")
+    puts()
       
   end
   
@@ -145,15 +162,30 @@ def find_reciprocal_best_hits(db1, db2)
     next unless best_hits2.has_key?(seq.definition.split('|')[0]) 
     
     report = factory2.query(seq)  # blast of query sequence in factory db
-    next unless report.hits[0] and report.hits[0].evalue < 1e-10 and report.hits[0].overlap > seq.length/2
+    
+    next unless report.hits[0] and report.hits[0].evalue < 1e-6
+    
+    if blast_case == 1 or blast_case == 2
+      percent_overlap = report.hits[0].overlap/seq.length.to_f
+      percent_identity = report.hits[0].identity/seq.length.to_f
+    
+    elsif blast_case == 3
+      percent_overlap = report.hits[0].overlap*3/seq.length.to_f
+      percent_identity = report.hits[0].identity*3/seq.length.to_f
       
+    end
+    
+    next unless percent_overlap >= 0.5 and percent_identity >= 0.3
+    
     id_query = seq.definition.split('|')[0]
     id_bh = report.hits[0].definition.split('|')[0]
     
     best_hits2[id_query] = id_bh   # stores the gene id correspondance
       
-    puts("Best hit found: #{id_query} -> #{id_bh}")  ## ESTO SE PUEDE QUITAR
-   
+    puts("Best hit found: #{id_query} -> #{id_bh}")
+    puts("%overlap: #{percent_overlap}\t%identity: #{percent_identity}\tevalue: #{report.hits[0].evalue}")
+    puts()
+    
   end
   
   # Checks reciprocal best hits in both best hits hashes
